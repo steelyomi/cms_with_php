084fff7ca552250a66aba72b0b78586133a0cd52
SOFSV12 <emmanuelsofuwa2@gmail.com>
2023-11-29 02:35:02 +0100

    COMMENTS

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -10,6 +10,8 @@
             <th>Tags</th>
             <th>Comments</th>
             <th>Date</th>
+            <th>Edit</th>
+            <th>Delete</th>
         </tr>
     </thread>
     <tbody>
@@ -71,6 +73,7 @@ if (isset($_GET['delete'])) {
 
     $query = "DELETE FROM posts WHERE post_id ={$the_post_id}";
     $delete_query = mysqli_query($connection, $query);
+    header("Location: posts.php");
 }
 
 
