62df03ba86afd7264a051d28ed7bcbc8f77a87f3
steelyomi <bmlasisi@gmail.com>
2023-12-05 17:16:11 +0100

    PROFILE

--- admin/index.php
+++ admin/index.php
@@ -16,7 +16,7 @@
 
                     <h1 class="page-header">
                         Welcome to admin
-                        <small>Author</small>
+                        <small><?php echo $_SESSION['username']; ?></small>
                     </h1>
 
                 </div>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -22,7 +22,7 @@
     <!-- Login-->
     <div class="well">
         <h4>Login</h4>
-        <form action="includes.login.php" method="post">
+        <form action="includes/login.php" method="post">
             <div class="form-group">
                 <input name="username" type="text" class="form-control" placeholder="Enter Username">
             </div>
